8a860dfb2e0cb88cd95a8804719cc728be029ec0
gghezzo <ginnyghezzo@gmail.com>
2015-08-09 08:45:50 -0400

    Chapter 4 pre-rewrite for generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse 		# overkill 
-from django.template import RequestContext, loader 	#overkill 
-from django.http import Http404				#overkill 
+from django.core.urlresolvers import reverse 
+from django.http import HttpResponse, HttpResponseRedirect
+
 from .models import Question 
+from django.template import RequestContext, loader 	#overkill 
+# from django.http import Http404				#overkill 
+
 
 # https://docs.djangoproject.com/en/1.8/intro/tutorial03/
 def index(request):
@@ -17,8 +20,19 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-	response = "You are looking atht eh results of question %s. "
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id): 
-	return HttpResponse("You are voting on question %s." % question_id)
+	p = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', { 
+			'question': p, 
+			'error_message': "Please select a choice",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
