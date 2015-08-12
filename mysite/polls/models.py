cf4fa13047138c2089ed271a4f994715bc3a37bf
gghezzo <ginnyghezzo@gmail.com>
2015-08-12 08:28:35 -0400

    Testing

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
-from django.db import models
+from django.db import models			
 import datetime 
 from django.utils import timezone
+# Note: My import is different then the guide https://docs.djangoproject.com/en/1.8/intro/tutorial05/
 
 # This is a tutorial from https://docs.djangoproject.com/en/1.8/intro/tutorial01/
 # I do not undersatnd the was_published_recently attributes. Methods? see list_display 
@@ -9,8 +10,11 @@ class Question(models.Model):
 	pub_date = models.DateTimeField('date published')
 	def __str__(self): 
 		return self.question_text
+
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 	was_published_recently.admin_order_field = 'pub_date'		# huh
 	was_published_recently.boolean = True
 	was_published_recently.short_description = 'Published recently'
