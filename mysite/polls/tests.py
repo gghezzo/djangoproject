444e983700c832947334e853d93b374c863b647f
gghezzo <ginnyghezzo@gmail.com>
2015-08-12 20:48:04 -0400

    Last of Testing

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -58,4 +58,15 @@ class QuestionViewTests(TestCase):
 		response = self.client.get(reverse('polls:index'))
 		self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question 2.>', '<Question: Past question 1.>'])
 
+class QuestionIndexDetailTests(TestCase):
+	def test_detail_view_with_a_future_question(self): 
+		# The detail view of a question with a pub_date om the future should get a 404 
+		future_question = create_question(question_text='Future question.', days=5)
+		response = self.client.get(reverse('polls:detail', args=(future_question.id,)))
+		self.assertEqual(response.status_code, 404)
+	def test_detail_view_with_a_past_questino(self):
+		# The detail view of a question with a pub_date in the past should display the questions's test. 
+		past_question = create_question(question_text='Past Question.', days=-5)
+		response = self.client.get(reverse('polls:detail', args=(past_question.id,)))
+		self.assertContains(response, past_question.question_text, status_code=200)
 
